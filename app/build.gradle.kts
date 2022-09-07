12be47cedc9db1421b5bfa3241d3112b78779ba4
Punk_Off <pankov.kirill1997@yandex.ru>
2022-09-07 22:46:26 +0300

    move to gradle kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,45 @@
+import ru.punkoff.buildsrc.Dependencies
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "ru.anb.friends"
+        minSdk = 23
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(Dependencies.CORE_KTX)
+    implementation(Dependencies.APP_COMPAT)
+    implementation(Dependencies.FRAGMENT_KTX)
+    implementation(Dependencies.CONSTRAINT_LAYOUT)
+    implementation(Dependencies.MATERIAL)
+}
---
+++ buildSrc/src/main/java/ru/punkoff/buildsrc/Versions.kt
@@ -0,0 +1,9 @@
+package ru.punkoff.buildsrc
+
+object Versions {
+    const val CORE_KTX_VERSION = "1.9.0"
+    const val APP_COMPAT_VERSION = "1.5.1"
+    const val FRAGMENT_KTX_VERSION = "1.5.2"
+    const val CONSTRAINT_LAYOUT_VERSION = "2.1.4"
+    const val MATERIAL_VERSION = "1.6.1"
+}
