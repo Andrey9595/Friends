264737b96fc14cfb34ba7ac7eccff5561ca6a680
Andrey9595 <anb95.95@mail.ru>
2022-12-15 19:42:54 +0300

    upgrade BottomSheet

--- app/src/main/java/ru/anb/friends/main/bottomsheet/BottomSheet.kt
+++ app/src/main/java/ru/anb/friends/main/bottomsheet/BottomSheet.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import ru.anb.friends.R
 
@@ -14,4 +15,14 @@ class BottomSheet : BottomSheetDialogFragment() {
     ): View? {
         return inflater.inflate(R.layout.bottob_shit_layout, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val buttonClose: ImageView = view.findViewById(R.id.close)
+        buttonClose.setOnClickListener{
+            dismiss()
+        }
+    }
+    override fun getTheme() = R.style.AppBottomSheetDialogTheme
+
 }
